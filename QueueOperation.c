0f871e3a50d35532452204b7e9048dd107ec3db5
Kundan Kumar <Kundan_kk52@outlook.com>
2018-09-20 09:59:46 +0530

    Update QueueOperation.c

--- QueueOperation.c
+++ QueueOperation.c
@@ -1,56 +1,70 @@
 #include<stdio.h>
-int queue[10];
-int front=0,rear=-1;
+#include<stdlib.h>
+
+#define size 10
+
 void enqueue(int);
 void dequeue();
 void display();
-void enqueue(int ele)
-{
- if(rear==9)
- {
-  printf("Queue is full\n");
- }
- else
- {
-  rear++;
-  queue[rear]=ele;
- }
-}
-void dequeue()
-{
- if(rear==-1)
- {
-  printf("Queue is empty\n");
- }
- else
- {
-  front++;
- }
-}
-void display()
-{
- for(int i=front;i<=rear;i++)
- {
-  printf("%d  ",queue[i]);
- }
- printf("\n");
-}
+
+int queue[size];
+int front = 0, rear = -1;
+
 void main()
 {
- int ele,ch;
- x:
- printf("Enter 1 for enqueue, 2 for dequeue, 3 for display\n");
- scanf("%d",&ch);
- switch(ch)
- {
-  case 1: printf("Enter an element\n");
-          scanf("%d",&ele);
-          enqueue(ele);
-          goto x;
-  case 2: dequeue();
-          goto x;
-  case 3: display();
-          goto x;
-  default: printf("Invalid");
- }
+  int choice,data;
+  while(1)
+   {
+    printf("Queue Operation \n");
+    printf("1.Enqueue \n2.Dequeue \n3.Display \n4.Exit \n");
+    scanf("%d",&choice);
+    switch(choice)
+     {
+      case 1 : printf("Enter an element : \n");
+               scanf("%d",&data);
+               enqueue(data);
+	       break;
+      case 2 : dequeue();
+               break;
+      case 3 : display();
+               break;
+      case 4 : exit(0);
+
+      default : printf("Invalid Option \n");
+     }
+   }
+}
+
+void enqueue(int data)
+{
+ if(front == 9)
+  {
+   printf("Queue is full \n");
+  }
+ else
+  {
+   rear++;
+   queue[rear] = data;
+  }
+}
+
+void dequeue()
+{
+ if(front == -1)
+  {
+   printf("Queue is empty \n");
+  }
+ else
+  {
+   front++;
+  }
+}
+
+void display()
+{
+ for(int i = front; i <= rear; i++)
+  {
+   printf("%d",queue[i]);
+  }
+  printf("\n");
 }
